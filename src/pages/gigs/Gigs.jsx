8c61365e12bd7f8eb710bce2326841ff2f486727
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-05-14 20:59:51 +0500

    initial project setup including pages

---
+++ src/pages/gigs/Gigs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Gigs() {
+  return (
+    <div>
+      <div className="Gigs"></div>
+    </div>
+  )
+}
---
+++ src/pages/myGigs/Mygigs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Mygigs() {
+  return (
+    <div>
+      <div className="Mygigs"></div>
+    </div>
+  )
+}
