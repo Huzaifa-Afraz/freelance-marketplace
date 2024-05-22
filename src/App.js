72de5ab8c6916604fedffeae16fe09b5818e6c5c
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-05-23 00:54:20 +0500

    almost finish navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
-
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar/Navbar";
+import './app.scss'
 function App() {
   return (
     <div className="App">
+   
    <Navbar/>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,35 @@
 import React from 'react'
-
+import'./navbar.scss'
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-       <header className=" bg-light">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container ">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-           
-            <div className="offcanvas offcanvas-start flex-grow-0" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-                <div className="offcanvas-header">
-                  <h5 className="offcanvas-title" id="offcanvasExampleLabel">Navbar</h5>
-                  <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            
-                          <span className="nav-link active" aria-current="page">Home</span>
-                        </li>
-                        <li className="nav-item">
-                          <span className="nav-link">Link</span>
-                        </li>
-                   
-                        <li className="nav-item">
-                          <span className="nav-link disabled">Disabled</span>
-                        </li>
-                      </ul>
-                </div>
-          </div>
+      <div className="navbar">
+        <div className="container">
+          <div className="logo">
+            {/* <Link to="/"> */}
+            <span className="text">PakFreelance</span>
+            {/* </Link> */}
+            <span className="dot">.</span>
+            </div>
+            <div className="links">
+              <span>Business</span>
+              <span>Explore</span>
+              <span>English</span>
+              <span>Sign in</span>
+              <span>Become a Seller</span>
+              <button>Join</button>
+              </div>
         </div>
-        </nav>
-    </header>
+        <hr />
+        <div className="container">
+
+        <div className="menu">
+          <span>jfj</span>
+          <span>fj</span>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
